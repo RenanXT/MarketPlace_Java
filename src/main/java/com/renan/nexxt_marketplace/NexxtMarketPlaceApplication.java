1065df6a26ae6beb48aee9ef210e19d9b42d6c6e
RyukXT <decryuk@gmail.com>
2026-07-17 10:46:33 -0300

    Criação do Projeto

---
+++ src/main/java/com/renan/nexxt_marketplace/NexxtMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.renan.nexxt_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexxtMarketPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexxtMarketPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/renan/nexxt_marketplace/NexxtMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.nexxt_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexxtMarketPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
